a9f3e8a3c445e20e02ebb65fbae0a66388e39ef9
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-11-16 16:38:03 +0530

    New Project

--- src/tasks/200200-DuplicateRegionException/commands.sh
+++ src/tasks/200200-DuplicateRegionException/commands.sh
@@ -1,8 +1,8 @@
 cd ../../..
 
-cd src/tasks/200200-DuplicateRegionException
+cd src/tasks/200240-ScopedRegionManager
 
-cd src/apps/200200-DuplicateRegionException
+cd src/apps/200240-ScopedRegionManager
 
 dotnet run --project ./SimplePrismShell/SimplePrismShell.csproj
 
